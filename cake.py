9a1482b5bcd3c1761d0e317a6f9e86cf894ad6c6
srinu2003 <tsrin2003@gmail.com>
2022-12-16 20:41:09 +0530

    hello

---
+++ cake.py
@@ -0,0 +1,23 @@
+def message(input):
+    if input==1:
+        print('Hello')
+    elif input==2:
+        print()
+    elif input==3:
+        print()
+    elif input==4:
+        print()
+    elif input==5:
+        print()
+    elif input==6:
+        print()
+    elif input==7:
+        print()
+    elif input==8:
+        print()
+    elif input==9:
+        print()
+    elif input==10:
+        print()
+    else:
+        print('invalied')
