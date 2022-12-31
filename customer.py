816e96e425643e3de5e996999a56ebe4897b5a8d
srinu2003 <tsrin2003@gmail.com>
2023-01-01 00:34:40 +0530

    jUST MODIFIED#1

--- customer.py
+++ customer.py
@@ -13,7 +13,7 @@ orderList = []
 # Chocolatey detected you are not running from an elevated command shell
 #  (cmd/powershell).
 
-#  You may experience errors - many functions/packages
+#  You may experience errors!! - many functions/packages
 #  require admin rights. Only advanced users should run choco w/out an
 #  elevated shell. When you open the command shell, you should ensure
 #  that you do so with "Run as Administrator" selected. If you are
